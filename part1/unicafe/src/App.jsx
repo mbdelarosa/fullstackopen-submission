848fb9bc89ad79aaf4b2e0c1fa16d968684424be
michelledr <michelled@poshpeanut.com>
2023-10-26 14:21:36 +0800

    Updated to display statistics using table, fixed typo on naming

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -10,7 +13,7 @@ const Statistics = ({ good, neutral, bad }) => {
   // compute for more statistics
   const total = good + neutral + bad
   const average = (good*1 + neutral*0 + bad*-1) / total
-  const percentage = (good / total) * 100
+  const positive = (good / total) * 100
 
   if (total == 0) {
     return (
@@ -24,12 +27,17 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="percentage" value={percentage + "%"} />
+
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + "%"} />
+        </tbody>
+      </table>
     </div>
   )
 }
