664a453af8df2b695b9d72c8425423ea58d7eb9a
michelledr <michelled@poshpeanut.com>
2023-10-26 13:52:36 +0800

    Added more statistics for exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // compute for more statistics
+  const total = good + neutral + bad
+  const average = (good*1 + neutral*0 + bad*-1) / total
+  const percentage = (good / total) * 100
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -17,6 +22,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>percentage {percentage}%</p>
     </div>
   )
 }
